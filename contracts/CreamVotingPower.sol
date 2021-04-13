2cb41fefc02aa313d9a95ab45e7058e794b88f93
Jeremy Yang <jeremy@cream.finance>
2021-04-13 14:57:24 +0800

    initial commit

---
+++ contracts/CreamVotingPower.sol
@@ -0,0 +1,12 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.7.0;
+
+
+contract CreamVotingPower {
+
+  //TODO: implement voting power from each sources.
+
+  function balanceOf(address account) returns (uint256) {
+    return 0;
+  }
+}
